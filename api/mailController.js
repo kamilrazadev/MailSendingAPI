ba521b60dc5daff4b0709f0d2542e586c3ba306a
M Kamil Raza <125785123+kamilrazadev@users.noreply.github.com>
2024-04-06 16:08:31 +0500

    Update mailController.js

--- api/mailController.js
+++ api/mailController.js
@@ -3,22 +3,6 @@ import { config } from "dotenv";
 config();
 
 export const handleSendMail = (req, res) => {
-  const {
-    gameTitle,
-    about,
-    isGameLive,
-    isGameTested,
-    noOfTeams,
-    gameExperience,
-    gameExperties,
-    newsletterSubscription,
-    email,
-    phone,
-    address,
-    url,
-    comment,
-  } = req.body;
-
   // Send Email
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -30,8 +14,8 @@ export const handleSendMail = (req, res) => {
 
   const mailOptions = {
     from: { name: "Game Changer", address: process.env.EMAIL },
-    to: "Submit@astranux.com, kamilrazadev@gmail.com", //enter reciever email here
-    subject: "Game Changer Record",
+    to: "example@gmail.com", //enter reciever email here
+    subject: "Your mail subject",
     html: `
     <!DOCTYPE html>
     <html lang="en">
@@ -46,107 +30,15 @@ export const handleSendMail = (req, res) => {
                 margin: 0;
                 padding: 0;
             }
-
-            .container {
-                padding: 20px;
-                border-radius: 10px;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                width: 90%;
+            .container h2, .container p {
+              text-align: center;
             }
-
-            h2 {
-                color: #005DFF;
-
-            }
-h4 {
-                color: #000000;
-margin: 0px;
-}
-
-            p {
-                color: #000000;
-margin: 0px;
-            }
-.fields-container {
-}
-.field > p {
-color: #7c7c7c;
-margin-bottom: 14px
-}
-
         </style>
     </head>
     <body>
         <div class="container">
-            <h2>Game Changer Form:</h2>
-<div class="fields-container">
-<div class="field">
-<h4>Game Title:</h4>
-<p>${gameTitle}</p>
-</div>
-
-<div class="field">
-<h4>About:</h4>
-<p>${about}</p>
-</div>
-
-<div class="field">
-<h4>Game live on play store:</h4>
-<p>${isGameLive}</p>
-</div>
-
-<div class="field">
-<h4>Game is tested:</h4>
-<p>${isGameTested}</p>
-</div>
-
-<div class="field">
-<h4>No of Teams:</h4>
-<p>${noOfTeams}</p>
-</div>
-
-<div class="field">
-<h4>Game Experience:</h4>
-<p>${gameExperience} years</p>
-</div>
-
-<div class="field">
-<h4>Game Experties:</h4>
-<p>${gameExperties}</p>
-</div>
-
-<div class="field">
-<h4>Newsletter Subscription:</h4>
-<p>${newsletterSubscription}</p>
-</div>
-
-<div class="field">
-<h4>Email:</h4>
-<p>${email}</p>
-</div>
-
-<div class="field">
-<h4>Phone:</h4>
-<p>${phone}</p>
-</div>
-
-<div class="field">
-<h4>Address:</h4>
-<p>${address}</p>
-</div>
-
-<div class="field">
-<h4>URL:</h4>
-<p>${url}</p>
-</div>
-
-<div class="field">
-<h4>Comment:</h4>
-<p>${comment}</p>
-</div>
-
-</div>
-
+            <h2>This is a dummy mail</h2>
+            <p>Thank you!</p>
         </div>
     </body>
     </html>
